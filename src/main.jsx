730e92d2b3bd162034f8e6045ddfc0c7f981a4c3
Stefano <stefanop21@outlook.es>
2023-11-24 21:39:35 -0500

    refactor(utils): Refactorización de estructura de carpetas

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './app';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import { extendTheme } from '@chakra-ui/react';
-
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-};
-
-const theme = extendTheme({ colors });
+import { theme } from './utils/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
